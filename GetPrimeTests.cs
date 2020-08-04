18351431a3a18aed23686e670bc2f60906a24f70
Dave Gedert <DaveGedert@QuickenLoans.com>
2020-08-04 07:29:13 -0400

    first commit

---
+++ GetPrimeTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace PrimeNumbers.Tests
+{
+    public class GetPrimeTests
+    {
+        [Theory]
+        [InlineData(7, Prime.PRIME)]
+        [InlineData(12, Prime.NOTPRIME)]
+        [InlineData(27, Prime.NOTPRIME)]
+        [InlineData(191, Prime.PRIME)]
+        [InlineData(1039, Prime.PRIME)]
+
+
+        public void TestAll(int userNum, Prime expected)
+        {
+            PrimeNumbers primeNum = new PrimeNumbers();
+            Prime final = (Prime)primeNum.GetPrime(userNum);
+
+            Assert.Equal(expected, final);
+        }
+    }
+}
