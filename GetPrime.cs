7d4fd15fda577dfa90759827644101fe200a215e
DaveGisCool <67699384+DaveGisCool@users.noreply.github.com>
2020-08-04 09:17:02 -0400

    Adding non-test files

---
+++ GetPrime.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimeNumbers
+{
+    public enum Prime
+    {
+        PRIME,
+        NOTPRIME
+    }
+
+    public class PrimeNumbers
+    {
+        public int GetPrime(int userNum)
+        {
+            int lastNum = 0;
+            int lastPrime = 3;
+            int primeCount = 2;
+
+            for (int index = 3; primeCount < userNum; index++)
+            {
+                if (index % 2 == 0)
+                {
+                    lastNum = index;
+                }
+                else if (index % 3 == 0)
+                {
+                    lastNum = index;
+                }
+                else if ((index / (Math.Truncate(Math.Sqrt(index)))) % 1 == 0)
+                {
+                    lastNum = index;
+                }
+                else
+                {
+                    lastPrime = index;
+                    primeCount++;
+                }
+            }
+            lastNum += 1;
+            return lastNum;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrimeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool keepLooping = true;
+            string anotherRound;
+
+            PrimeNumbers test = new PrimeNumbers();
+
+            Console.WriteLine($"Let's locate some primes!\n\nThis application will find you any prime, in order, from first prime number on.");
+
+            while (keepLooping)
+            {
+                Console.Write("\nWhich prime number are you looking for?");
+                int userInput = Int32.Parse(Console.ReadLine());
+
+                int nth = test.GetPrime(userInput);
+
+                Console.WriteLine($"\nThe number {userInput} prime is {nth}.");
+                do
+                {
+                    Console.WriteLine($"\nDo you want to find another prime number? (y/n):");
+                    anotherRound = Console.ReadLine().ToLower();
+                    if (anotherRound == "n")
+                    { keepLooping = false; }
+                    else if (anotherRound == "y")
+                    { continue; }
+                    else
+                    {
+                        Console.WriteLine("That is not a valid choice. Select y or n ");
+                    }
+                } while (anotherRound != "y" && anotherRound != "n"); 
+            }
+        }
+    }
+}
